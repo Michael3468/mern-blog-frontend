cdc356abadda8faa72c95b5c1a9a59e15b1e0bf5
Mikhail <mikhail3468@yandex.ru>
2023-01-19 11:17:07 +0300

    feat: add store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App';
 import CssBaseline from '@mui/material/CssBaseline';
 
@@ -8,15 +9,19 @@ import './index.scss';
 import { ThemeProvider } from '@mui/material';
 import { theme } from './theme';
 
+import store from './redux/store';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
+  <>
     <CssBaseline />
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </BrowserRouter>
     </ThemeProvider>
-  </React.StrictMode>,
+  </>,
 );
