0a15cd171c010689c85c2537fb773802afce3015
Mikhail <mikhail3468@yandex.ru>
2023-02-09 09:44:37 +0300

    refactor(header.jsx): change imports order

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom';
-import Button from '@mui/material/Button';
-
-import styles from './Header.module.scss';
-import Container from '@mui/material/Container';
 import { useDispatch, useSelector } from 'react-redux';
 import { logout, selectIsAuth } from '../../redux/slices/auth';
+
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+
+import styles from './Header.module.scss';
 
 export const Header = () => {
   const dispatch = useDispatch();
